3ba7082187e0f70f5ca1c7ffb4238b3c7ea9e532
Hyeunseung <hyeunseung03@gmail.com>
2023-03-28 11:17:42 +0900

    initial commit

---
+++ src/main/java/ourminiprojects/mykiosk/MyKioskApplication.java
@@ -0,0 +1,13 @@
+package ourminiprojects.mykiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKioskApplication.class, args);
+    }
+
+}
